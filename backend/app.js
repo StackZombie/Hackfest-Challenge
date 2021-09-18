b79752dd0befc13519b14c91b14a89266366548d
MuhammaadSaad <muhammadsaad2387@gmail.com>
2021-09-18 23:59:34 +0500

    changes

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use("/user",userRoute);
-app.use("/channel",channelRoute);s
+app.use("/channel",channelRoute);
 
 
 app.listen(PORT , ()=>{
