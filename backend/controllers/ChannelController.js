22acd5bd6c44970c3f52b97d1ee97fd33bf7969e
Muammad Kurram raza <mraza@NIS-PK492.local>
2021-09-19 15:51:44 +0500

    Adding queries

--- backend/controllers/ChannelController.js
+++ backend/controllers/ChannelController.js
@@ -1,13 +1,18 @@
+//const redisClient = require("../dbconnection/RedisConnection");
+
+
+
 //get all channels
 const list = async()=>{
     try{
         await console.log("finding list");
+       // const result = await redisClient.smembers("channels");
         if(1)//founnd
         {
             return({
                 status:true,
                 message:"Channle List",
-                data:"[channel list]",
+                data:"[result]",
                 code :200
 
             })
@@ -27,14 +32,23 @@ const create = async(channelName)=>{
         try{
             await console.log("channel name",channelName);
             //check either the channel already exist or not
+            //const exist = await redisClient.smembers("channels",channelName);
             // if exist than return false or else create channel and return true
-
+            if(1 /*exist is true */){
             return({
                 status:true,
-                data:"channel name",
-                message:"channel created",
+                data:channelName,
+                message:"channel Already Exist",
                 code:200
-            })
+            })} else{
+                const result = redisClient.sadd("channels",channelName);
+                return({
+                    status:true,
+                    data:"{result}",
+                    message:"New Channel Created",
+                    code:200
+                })
+            }
         }catch(err){
             return({
                 status:false,
@@ -47,9 +61,10 @@ const create = async(channelName)=>{
 const joinChannel = async(channelName, userName)=>{
     try{
         await console.log("Channel ",channelName,userName);
+       // const join = await redisClient.xadd("channel:"+channelName+"* type join username",userName);
         return({
             message:"Channel Joined Successfully",
-            data : "[message-id]",
+            data : "join.messageid",
             status:true,
             code:200
         })
@@ -65,9 +80,11 @@ const joinChannel = async(channelName, userName)=>{
 const leaveChannel = async(channelName,userName)=>{
     try{
         await console.log("Channel ",channelName,userName);
+
+       // const leave = await redisClient.xadd("channel:"+channelName+"* type leave username",userName);
         return({
             message:"Channel Left Successfully",
-            data : "[message-id]",
+            data : "leave.messageid",
             status:true,
             code:200
         })
